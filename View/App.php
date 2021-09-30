35f471963828d67a626cfbf2495eada982fbde99
aJlino82 <adailton_jr@hotmail.com>
2021-09-30 14:16:53 -0300

    Melhorando o visual e as classes

--- View/App.php
+++ View/App.php
@@ -1,27 +1,41 @@
 <!doctype html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     <title>PHP SIS-PAC</title>
 </head>
+
 <body>
-<div class="container">
+    <div class="container">
+        <header>
+            <nav class="light-red lighten-1" role="navigation">
+                <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">SIS-PAC</a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="#">Navbar Link</a></li>
+                    </ul>
 
+                    <ul id="nav-mobile" class="sidenav">
+                        <li><a href="#">Navbar Link</a></li>
+                    </ul>
+                    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                </div>
+            </nav>
 
-    <blockquote>
-        <h4>Exibindo dados da view.</h4>
-    </blockquote>
+        </header>
+        <div class="row">
+            <div class="card-panel">
+                <h5>Vá para o login.</h5>
+                <a href="/?page=login">Login</a>
+            </div>
+        </div>
 
-    <div class="card-panel">
-        <h5>Vá para o login.</h5>
-        <a href="/?page=login">Login</a>
     </div>
 
-</div>
-
 </body>
+
 </html>
--- View/Login.php
+++ View/Login.php
@@ -1,30 +1,58 @@
 <!doctype html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     <title>Página de Login:</title>
 </head>
+
 <body>
+    <header>
+        <nav class="light-red lighten-1" role="navigation">
+            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">SIS-PAC</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#">Navbar Link</a></li>
+                </ul>
 
-<div class="row">
-    <form class="col s12">
+                <ul id="nav-mobile" class="sidenav">
+                    <li><a href="#">Navbar Link</a></li>
+                </ul>
+                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+
+    </header>
+    <div class="container">
         <div class="row">
-            <div class="input-field col s6">
-                <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                <label for="first_name">First Name</label>
+            <h4 class="row center">Login</h4>
+            <br>
+            <div class="col s3"></div>
+            <div class="col s6">
+                <div class="input-field ">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-field">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                </div>
             </div>
-            <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
-                <label for="last_name">Last Name</label>
-            </div>
+            <div class="col s3"></div>
+        </div>
+    </div>
 
-    </form>
 
-</div>
+
+
 
 </body>
+
 </html>
